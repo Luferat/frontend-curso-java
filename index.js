b4df5c3810993f1488f454a3fe106ee0fd176e4d
Luferat <contato@luferat.net>
2023-04-05 21:33:24 -0300

    loadpage ok

--- index.js
+++ index.js
@@ -2,6 +2,28 @@ $(document).ready(myApp)
 
 function myApp() {
 
-    console.log('Funciona!')
+    // Carrega a página inicial.
+    loadpage('biscoit')
+
+}
+
+// Carrega uma página.
+/* O caminho final da página é passado como parâmetro da função. */
+function loadpage(page) {
+
+    // Monta os caminhos para os componentes da página solicitada.
+    const path = {
+        html: `/pages/${page}/index.html`,
+        css: `/pages/${page}/index.css`,
+        js: `/pages/${page}/index.js`
+    }
+
+    $.get(path.html)
+        .done((data) => {
+            $('main').html(data)
+        })
+        .fail((error) => {
+            loadpage('e404')
+        })
 
 }
