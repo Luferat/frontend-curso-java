5ac56e02f6209c2565d29d64ddda44e831b12a6b
Luferat <contato@luferat.net>
2023-04-13 16:48:48 -0300

    Ajustes na API

--- api/index.js
+++ api/index.js
@@ -1,33 +1,106 @@
 /**
- * Middleware fo JSON-SERVER para mudar o response de POST.
- * Refereces: https://stackoverflow.com/questions/58297415/node-json-server-returning-mock-post-response
+ * JSON-Server Fake API
+ * By Luferat
+ * MIT License
+ * 
+ * ATENÇÃO! Lembre-se que este aplicativo será substituído pela nossa própria
+ * API REST no momento oportuno.
  **/
 
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
+// Carrega e inicializa o json-server e suas dependências.
+const jsonServer = require('json-server')
 
-server.use(jsonServer.bodyParser);
-server.use(middlewares);
+// Cria um novo aplicativo (objeto) json-server.
+const server = jsonServer.create()
 
-// Custom middleware to access POST methids.
-// Can be customized for other HTTP method as well.
+// Define o arquivo "JSON" (banco de dados fake).
+const router = jsonServer.router('db.json')
+
+// Carrega o recurso de "middlewares" do json-server.
+const middlewares = jsonServer.defaults()
+
+// Ativa o recurso de "middlewares" do json-server.
+server.use(middlewares)
+
+// Define a porta HTTP do json-server.
+const port = 3000
+
+/**
+ * Extrai os dados da requisição para tratamento da API.
+ * Isso é necessário para os métodos POST e PUT porque eles enviam dados para 
+ * a API na forma de JSON que são convertidos (parse) para Object.
+ **/
+server.use(jsonServer.bodyParser)
+
+/**
+ * Intercepta e altera os dados vindos do front-end antes de enviá-los para a 
+ * API json-server.
+ */
 server.use((req, res, next) => {
-  console.log("POST request listener");
-  const body = req.body;
-  console.log(body);
-  if (req.method === "POST") {
-    // If the method is a POST echo back the name from request body
-    res.json({ status: 'success', data: 'Contato enviado com sucesso'});
-  }else{
-      //Not a post request. Let db.json handle it
-      next();
-  }  
-});
 
-server.use(router);
+  // Se esta enviando um novo contato...
+  if (req.method == 'POST' && req.path == '/contacts') {
 
-server.listen(3000, () => {
-  console.log("JSON Server is running on port 3000");
-});
+    // Obtém a data atual do sistema.
+    const today = new Date()
+
+    // Formata a data para 'system date' (aaaa-mm-dd hh:ii:ss).
+    sysdate = today.toISOString().replace('T', ' ').split('.')[0]
+
+    // Inclui a data no 'body' da requisição.
+    req.body.date = sysdate
+  }
+
+  // Encerra 'server.use()', gravando as alterações.
+  next()
+})
+
+/**
+ * Renderiza a 'view' do json-server.
+ * Isso altera a saída padrão do json-server para customizá-la.
+ **/
+router.render = (req, res, next) => {
+
+  // Se um novo contato foi enviado...
+  if (req.method == 'POST' && req.path == '/contacts') {
+
+    // Se o novo contato foi gravado com sucesso...
+    if (res.locals.data.id > 0) {
+
+      /** 
+       * Responde com o JSON.
+       * O importante aqui é { "status" : "success" }, o restante é opcional.
+       **/
+      res.json({
+        status: 'success',
+        data: {
+          message: 'Contato salvo',
+          HTTPcode: res.statusCode,
+          id: res.locals.data.id
+        }
+      })
+
+      // Se houve falha ao gravar contato...
+    } else {
+
+      /** 
+       * Responde com o JSON.
+       * O importante aqui é { "status" : "error" }, o restante é opcional.
+       **/
+      res.json({
+        status: 'error',
+        data: {
+          message: 'Falha ao gravar contato'
+        }
+      })
+    }
+  }
+}
+
+// Executa a API do json-server.
+server.use(router)
+
+// Escutando requisições.
+server.listen(port, () => {
+  console.log(`JSON Server: http://localhost:${port}/`)
+})
--- pages/contacts/index.js
+++ pages/contacts/index.js
@@ -1,40 +1,76 @@
+/**
+ * Contacts Control
+ * By Luferat
+ * MIT License 
+ **/
+
+// Executa 'myContacts()' quando o documento estiver pronto.
 $(document).ready(myContacts)
 
 /**
  * Função principal da página "contacts".
  **/
 function myContacts() {
+
     /**
      * Altera o título da página quando 'contacts' for acessada.
+     * Usa a função sendContact() definida em '/index.js'.
      **/
     changeTitle('Faça contato')
 
+    /**
+     * Observer do formulário de contatos.
+     * Quando o formulário for enviado (onsubmit), executa a função
+     * sendContact().
+     */
     $(document).on('submit', '#cForm', sendContact)
 
 }
 
+/**
+ * Processa o envio do formulário de contatos.
+ */
 function sendContact(ev) {
 
+    // Bloqueia a ação normal do HTML, de enviar o formulário.
+    ev.preventDefault()
+
+    // Cria o objeto que vai armazenar dados de saída para a API.
     var formJSON = {}
 
+    // Armazena o formulário como um objeto.
     const formData = new FormData(ev.target);
 
+    // Obtém cada campo do formulário.
     formData.forEach((value, key) => {
-        formJSON[key] = value
+
+        // Sanitiza o valor do campo e armazena no atributo do JSON.
+        formJSON[key] = value.trim()
+
+        // Atualiza o campo sanitizado, no formulário.
+        $('#' + key).val(formJSON[key])
     })
 
-    $.post('http://localhost:3000/contacts', formJSON)
-    .done((data) => {
-        if(data.status == 'success') {
-            var firstName = formJSON.name.split(' ')[0]
-            var feedback = `
-                <h3>Olá ${firstName}!</h3>
-                <p>Seu contato foi enviado com sucesso.</p>
-                <p>Obrigado...</p>
-            `
+    // Envia os dados do formulário para a API.
+    $.post(app.apiContactsURL, formJSON)
+        .done((data) => {
+            var feedback;
+            if (data.status == 'success') {
+                var firstName = formJSON.name.split(' ')[0]
+                feedback = `
+                    <h3>Olá ${firstName}!</h3>
+                    <p>Seu contato foi enviado com sucesso.</p>
+                    <p>Obrigado...</p>
+                `
+            } else {
+                feedback = `
+                    <h3>Oooops!</h3>
+                    <p>Não foi possível enviar seu contato. Ocorreu uma falha no servidor.</p>
+                    <p><em><code>${data.data.message}</code></em></p>
+                `
+            }
             $('#cForm').html(feedback)
-        }
-    })
+        })
 
     return false
 }
