1f2c7611a4b922e94d1f76dc9e7389cb9587a8ec
André Luferat <33375030+Luferat@users.noreply.github.com>
2023-05-12 09:24:52 -0300

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -2,6 +2,7 @@ $(document).ready(myAbout)
 
 function myAbout() {
     changeTitle(`Sobre o ${app.siteName}`)
+    delete sessionStorage.userId
     $('#siteName').html(app.siteName)
     getUsersTeam()
 }
